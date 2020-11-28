dcde4771542b67b35038e15419a435b39279729d
Benz <Benzeman097-nafazbenzema@gmail.com>
2020-11-28 22:52:23 +0530

    jwt token is implemented

--- authorization-server/src/main/java/com/benz/authorization/server/api/config/AuthorizationServerConfig.java
+++ authorization-server/src/main/java/com/benz/authorization/server/api/config/AuthorizationServerConfig.java
@@ -1,41 +1,60 @@
 package com.benz.authorization.server.api.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.benz.authorization.server.api.exception.CustomOAuthException;
+import com.benz.authorization.server.api.security.SecurityProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import javax.sql.DataSource;
+import java.security.KeyPair;
 
 @Configuration
 @EnableAuthorizationServer
+@EnableConfigurationProperties(SecurityProperties.class)
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
     private DataSource dataSource;
     private PasswordEncoder passwordEncoder;
     private AuthenticationManager authenticationManager;
     private UserDetailsService userDetailsService;
+    private SecurityProperties securityProperties;
+    private TokenStore tokenStore;
 
     public AuthorizationServerConfig(DataSource dataSource,PasswordEncoder passwordEncoder,
-                                     AuthenticationManager authenticationManager,UserDetailsService userDetailsService)
+                                     AuthenticationManager authenticationManager,UserDetailsService userDetailsService,
+                                     SecurityProperties securityProperties)
     {
         this.dataSource=dataSource;
         this.passwordEncoder=passwordEncoder;
         this.authenticationManager=authenticationManager;
         this.userDetailsService=userDetailsService;
+        this.securityProperties=securityProperties;
     }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-         security.checkTokenAccess("permitAll()").tokenKeyAccess("permitAll()");
+         //security.checkTokenAccess("permitAll()").tokenKeyAccess("permitAll()");
+        security.checkTokenAccess("isAuthenticated()").tokenKeyAccess("isAuthenticated()");
          //in Production, you have to make both as isAuthenticated()
     }
+
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -45,6 +64,59 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsService);
+                .userDetailsService(userDetailsService)
+        .exceptionTranslator(exception->{
+            if(exception instanceof OAuth2Exception)
+            {
+                OAuth2Exception oAuth2Exception=(OAuth2Exception) exception;
+                return ResponseEntity.status(oAuth2Exception.getHttpErrorCode())
+                        .body(new CustomOAuthException(oAuth2Exception.getMessage()));
+            }else
+                throw exception;
+        }).accessTokenConverter(jwtAccessTokenConverter())
+                .tokenStore(tokenStore());
+    }
+
+    @Bean
+    public TokenStore tokenStore()
+    {
+          if(tokenStore==null)
+              tokenStore = new JwtTokenStore(jwtAccessTokenConverter());
+          return tokenStore;
+    }
+
+    @Bean
+    public DefaultTokenServices defaultTokenServices(TokenStore tokenStore,ClientDetailsService clientDetailsService)
+    {
+        DefaultTokenServices tokenServices=new DefaultTokenServices();
+        tokenServices.setReuseRefreshToken(true);
+        tokenServices.setTokenStore(tokenStore);
+        tokenServices.setClientDetailsService(clientDetailsService);
+        tokenServices.setAuthenticationManager(authenticationManager);
+        return tokenServices;
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter()
+    {
+        SecurityProperties.JwtProperties jwtProperties =securityProperties.getJwt();
+
+        KeyPair keyPair = keyPair(jwtProperties,keyStoreKeyFactory(jwtProperties));
+
+        JwtAccessTokenConverter accessTokenConverter=new JwtAccessTokenConverter();
+
+        accessTokenConverter.setKeyPair(keyPair);
+
+        return accessTokenConverter;
+    }
+
+    private KeyPair keyPair(SecurityProperties.JwtProperties jwtProperties, KeyStoreKeyFactory keyStoreKeyFactory)
+    {
+        return keyStoreKeyFactory.getKeyPair(jwtProperties.getKeyPairAlias(),jwtProperties.getKeyPairPassword().toCharArray());
+    }
+
+    private KeyStoreKeyFactory keyStoreKeyFactory(SecurityProperties.JwtProperties jwtProperties)
+    {
+      return new KeyStoreKeyFactory(jwtProperties.getKeyStore(),jwtProperties.getKeyStorePassword().toCharArray());
     }
 }
---
+++ authorization-server/src/main/java/com/benz/authorization/server/api/exception/CustomOAuthException.java
@@ -0,0 +1,13 @@
+package com.benz.authorization.server.api.exception;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
+
+@JsonSerialize(using = CustomOAuthExceptionSerializer.class)
+public class CustomOAuthException extends OAuth2Exception {
+
+    public CustomOAuthException(String msg)
+    {
+        super(msg);
+    }
+}
