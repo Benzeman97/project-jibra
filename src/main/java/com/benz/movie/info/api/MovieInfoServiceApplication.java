7bf5eb58185afdb7f899f86b42cdc4641a6b79b5
Benz <Benzeman097-nafazbenzema@gmail.com>
2021-03-17 14:58:02 +0530

    movie-info-service initialized

---
+++ src/main/java/com/benz/movie/info/api/MovieInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.benz.movie.info.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+
+}
